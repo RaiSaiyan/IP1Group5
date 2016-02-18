f60dfd907b7a6341d50421e3c29e2e83cd4a1e9e
benmadine <benmadine@gmail.com>
2016-02-18 15:44:00 +0000

    stuff

---
+++ IPProject1/Assets/Scripts/FoodSwitchScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodSwitchScript : MonoBehaviour {
+
+	public GameObject foodObject;
+	public Sprite carrotImage;
+	public Sprite meatImage;
+	
+	void Start()
+	{
+
+	}
+
+	public void CarrotSwitch()
+	{
+		foodObject.GetComponent<SpriteRenderer> ().sprite = carrotImage;
+	}
+
+	public void MeatSwitch()
+	{
+		foodObject.GetComponent<SpriteRenderer> ().sprite = meatImage;
+	}
+}
---
+++ IPProject1/Assets/Scripts/goneOnTouch.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goneOnTouch : MonoBehaviour {
+
+	bool checkThis = false;
+	// Use this for initialization
+	void Start () 
+	{
+		//checkThis = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (checkThis) 
+		{
+			transform.Translate(Vector3.left);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		// Checking for the appropriate tag
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			Destroy(gameObject);
+			Debug.Log("Animal and Food have been destroyed.");
+		}
+	}
+
+	public void changeBool()
+	{
+		checkThis = true;
+		//Debug.Log ("HERE");
+	}
+}
